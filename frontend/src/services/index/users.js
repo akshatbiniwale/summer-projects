22876062e9c01ca82a16857ab0c89cb1fdee2f70
Akshat Biniwale <akshatbiniwale@gmail.com>
2023-07-19 12:00:17 +0530

    Bug fixes

--- frontend/src/services/index/users.js
+++ frontend/src/services/index/users.js
@@ -9,9 +9,9 @@ export const signUp = async ({ name, email, password }) => {
         });
         return data;
     } catch (error) {
-        if(error.response && error.response.data.message){
+        if (error.response && error.response.data.message) {
             throw new Error(error.response.data.message);
         }
-        throw new Error(error.message)
+        throw new Error(error.message);
     }
 };

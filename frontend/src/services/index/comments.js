6511fc41f9dd389a2ff4f61fcbf4f555913ba42d
Akshat Biniwale <akshatbiniwale@gmail.com>
2025-04-18 14:34:39 +0530

    post categories frontend service complete

--- frontend/src/services/index/comments.js
+++ frontend/src/services/index/comments.js
@@ -80,3 +80,28 @@ export const deleteComment = async ({ commentId, token }) => {
         throw new Error(error.message);
     }
 };
+
+export const getAllComments = async (
+	token,
+	searchKeyword = "",
+	page = 1,
+	limit = 10
+) => {
+	try {
+		const config = {
+			headers: {
+				Authorization: `Bearer ${token}`,
+			},
+		};
+
+		const { data, headers } = await axios.get(
+			`/api/comments?searchKeyword=${searchKeyword}&page=${page}&limit=${limit}`,
+			config
+		);
+		return { data, headers };
+	} catch (error) {
+		if (error.response && error.response.data.message)
+			throw new Error(error.response.data.message);
+		throw new Error(error.message);
+	}
+};

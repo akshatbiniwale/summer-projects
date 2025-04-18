77858aa3fc7d387281d87ef6e3f5094a91145326
Akshat Biniwale <akshatbiniwale@gmail.com>
2025-04-18 10:30:39 +0530

    post categories controller add and other controllers update

--- backend/routes/commentRoutes.js
+++ backend/routes/commentRoutes.js
@@ -1,16 +1,19 @@
 const express = require("express");
 const router = express.Router();
-const { authGuard } = require("../middleware/authMiddleware");
+const { authGuard, adminGuard } = require("../middleware/authMiddleware");
 const {
-    createComment,
-    updateComment,
-    deleteComment,
+	createComment,
+	updateComment,
+	deleteComment,
+    getAllComments
 } = require("../controllers/commentControllers");
 
-router.post("/", authGuard, createComment);
 router
-    .route("/:commentId")
-    .put(authGuard, updateComment)
-    .delete(authGuard, deleteComment);
+	.post("/", authGuard, createComment)
+	.get(authGuard, adminGuard, getAllComments);
+router
+	.route("/:commentId")
+	.put(authGuard, updateComment)
+	.delete(authGuard, deleteComment);
 
 module.exports = router;
